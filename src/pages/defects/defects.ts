e5bf160d4857455f3a4aaadfc8021bef8986fc3c
Popat Kharat <kharatps02@gmail.com>
2018-02-20 18:47:47 +0530

    - Data binding for defect detail page

--- src/pages/defects/defects.ts
+++ src/pages/defects/defects.ts
@@ -7,12 +7,40 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'defects.html',
 })
 export class DefectsPage {
-
+  private defects: Array<any>;
+  private selectedDefectId: string
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DefectsPage');
+    this.init();
+  }
+
+  init() {
+    this.defects = [
+      {
+        id: 'd1', name: 'Water leak in kitchen think 1',
+        description: 'Water leak in kitchen think',
+        zone: 'Kitchen',
+        subcontractor: 'PlumerBER LTD',
+        due_date: '2/3/3028',
+        stage_name: 'First Stage',
+        inpsection_line_item: 'Inspector form',
+        found_by: 'John',
+        fix_on_stage: 'Last',
+        by_sunc: 'PlumberBER PVT LTD',
+        fix_by: 'Jhon'
+      },
+      { id: 'd2', name: 'Water leak in kitchen think 2' },
+      { id: 'd3', name: 'Water leak in kitchen think 3' },
+      { id: 'd4', name: 'Water leak in kitchen think 3' }
+    ];
+    this.selectedDefectId = this.defects[0].id;
+  }
+
+  onDefectsChange() {
+
   }
 
 }
