b1c4148467b5daecba4697208734e561c80ccaba
Suyog <suyog.navgale@gmail.com>
2018-02-20 13:43:22 +0530

    - Inspection Header implemented

--- src/pages/inspections/inspections.ts
+++ src/pages/inspections/inspections.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { StageProvider, ILocation, IStage } from '../../providers/stage-service/stage-service';
 
 @IonicPage()
 @Component({
@@ -8,16 +9,30 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class InspectionsPage {
   private expandCol: string = 'beforeStart';
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private locations: Array<ILocation>;
+  private selectedLocationId: string;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private stageProvider: StageProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad InspectionsPage');
+    this.init();
+  }
+  init() {
+    this.locations = this.stageProvider.getLocations();
+    this.selectedLocationId = this.locations[0].id;
+    this.stages = this.stageProvider.getStagesByLocationId(this.selectedLocationId);
+  }
+
+  onLocationChange() {
+    this.stages = this.stageProvider.getStagesByLocationId(this.selectedLocationId);
+  }
+
+  navigateTo(toPageName, options) {
+    this.navCtrl.push(toPageName, options);
   }
   expandCollapse(workstage){
     this.expandCol = workstage;
-
   }
 
 }
